3ad0724c9d125d59768c58010ffc1c8a7a20bed5
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2021-10-03 16:19:03 +0800

    Added the SubModules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-calculator';
+  title = 'SIMPLE ANGULAR CALCULATOR';
+  firstNum = 0;
+  secondNum = 0;
+  result = 0;
+  theme = 'Dark Mode';
+
+  setOperation(value: string){
+    const operation = parseInt(value);
+
+    switch(operation){
+      case 1 :this.addition();break;
+      case 2: this.subtraction();break;
+      case 3: this.multiplication();break;
+      case 4: this.division();break;
+    }
+
+  }
+
+
+  setFirstNum(value: string){
+    const parsedInt = parseInt(value);
+
+    if (!isNaN(parsedInt)){
+      this.firstNum = parsedInt;
+    }
+  }
+
+  setSecondNum(value: string){
+    const parsedInt = parseInt(value);
+
+    if (!isNaN(parsedInt)){
+      this.secondNum = parsedInt;
+    }
+  }
+
+  addition(){
+    this.result = this.firstNum + this.secondNum;
+  }
+  subtraction(){
+    this.result = this.firstNum - this.secondNum;
+  }
+  multiplication(){
+    this.result = this.firstNum * this.secondNum;
+  }
+  division(){
+    this.result = this.firstNum / this.secondNum;
+  }
+
+  onClick(){
+    if(this.theme === 'Light Mode')
+      this.theme = 'Dark Mode';
+    else
+      this.theme = 'Light Mode';
+  }
+
+
+
+
 }
